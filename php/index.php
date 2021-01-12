23b33899e4cab5580cc16aa22ef20b8c2baaa87b
Honcus <71695306+Honcus@users.noreply.github.com>
2021-01-12 12:07:44 +0100

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,70 @@
+<<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'Hello World!<br /><br />';
+        $pokus=true; 
+$nazev="Michal Honek"; 
+$cislo=18; 
+$cena=30.4;
+$hop = $nazev . $cena;
+echo $pokus;
+echo '<br /><br />';
+echo $nazev;
+echo '<br /><br />';
+echo $cislo;
+echo '<br /><br />';
+echo $cena;
+echo '<br /><br />';
+echo '<strong>odstavec</strong>';
+echo '<br /><br />';
+
+print 'Hello World!';
+print $pokus;
+print '<br /><br />';
+print $nazev;
+print'<br /><br />';
+print $cislo;
+print '<br /><br />';
+print $cena;
+print '<br /><br />';
+echo '<img src="bart.jpg" alt"pokus">';
+
+print '<br /><br />';
+echo '<strong>\\</strong>';
+print '<br /><br />';
+
+$b = 8;
+$a = 16;
+
+var_dump ($a+$b);
+var_dump ($a-$b);
+var_dump ($a/$b);
+var_dump ($a*$b);
+var_dump ($a++);
+var_dump ($a--);
+var_dump ($a+=$b);
+var_dump ($a-=$b);
+var_dump ($a*=$b);
+var_dump ($a/=$b);
+var_dump ($a==$b);
+var_dump ($a!=$b);
+var_dump ($a>$b);
+var_dump ($a<$b);
+var_dump ($a||$b);
+var_dump ($a&&$b);
+var_dump (!$a);
+
+
+        ?>
+    </body>
+</html>
